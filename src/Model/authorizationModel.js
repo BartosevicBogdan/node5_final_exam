595f0531f1b194344b99126f162ac75c75e540e2
BartosevicBogdan <95608837+BartosevicBogdan@users.noreply.github.com>
2022-03-16 17:03:40 +0300

    authorization model refactoring, simplified functions moved to dbHelper

--- src/Model/authorizationModel.js
+++ src/Model/authorizationModel.js
@@ -1,29 +1,10 @@
-const mysql = require('mysql2/promise');
-const dbConfig = require('../../configuration/dbConfig');
+const { sqlExecute_ObjectValues } = require('../utils/dbHelper');
 
 const table = 'users';
 
-function catchErrorObject(catchError, sqlRequestArguments) {
-  const obj = {
-    success: false,
-    message: 'database catched error',
-    sqlState: catchError.sqlState,
-    sqlMessage: catchError.sqlMessage,
-    enteredValues: sqlRequestArguments,
-  };
-  return obj;
-}
-
 async function registrationRequestDB(validValues) {
-  try {
-    const conn = await mysql.createConnection(dbConfig);
-    const sql = `INSERT INTO ${table} (full_name, email, password) VALUES (?, ?, ?)`;
-    const [fields] = await conn.execute(sql, Object.values(validValues));
-    await conn.close();
-    return fields;
-  } catch (error) {
-    return catchErrorObject(error, validValues);
-  }
+  const sql = `INSERT INTO ${table} (full_name, email, password) VALUES (?, ?, ?)`;
+  return sqlExecute_ObjectValues(sql, validValues);
 }
 
 module.exports = {
