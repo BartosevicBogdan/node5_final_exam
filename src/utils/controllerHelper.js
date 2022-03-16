c2075811ded017133c1984d75fe9afa83b6cb3d8
BartosevicBogdan <95608837+BartosevicBogdan@users.noreply.github.com>
2022-03-16 19:53:23 +0300

    verifyPass, cretaToken added to controllerHelper

--- src/utils/controllerHelper.js
+++ src/utils/controllerHelper.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../../configuration/jwtConfig');
 
 function successResponce(res, data, status = 200) {
   res.status(status).json({
@@ -16,8 +18,20 @@ function failResponce(res, err = 'Something went wrong', status = 500) {
 function hashPass(plainPassword) {
   return bcrypt.hashSync(plainPassword, 10);
 }
+function verifyPass(checkPassword, truePassword) {
+  return checkPassword && bcrypt.compareSync(checkPassword, truePassword);
+}
+
+function createToken(dataObjToToken, expiresIn) {
+  return jwt.sign(dataObjToToken, jwtSecret, {
+    expiresIn: expiresIn,
+  });
+}
+
 module.exports = {
   successResponce,
   failResponce,
   hashPass,
+  verifyPass,
+  createToken,
 };
