60c8d05e694825a7eb203409415547235d01b2a9
BartosevicBogdan <95608837+BartosevicBogdan@users.noreply.github.com>
2022-03-17 00:22:27 +0300

    added isLoggedIn middleware

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -15,4 +15,14 @@ async function validateLoginForm(req, res, next) {
   validateHandler(login_validationSchema, req.body, next, res);
 }
 
-module.exports = { validateRegistrationForm, validateLoginForm };
+async function isLoggedIn(req, res, next) {
+  try {
+    const token = req.headers.authorization?.split(' ')[1];
+    req.token = jwt.verify(token, jwtSecret);
+    next();
+  } catch (error) {
+    res.status(401).send({ error: 'invalid token' });
+  }
+}
+
+module.exports = { validateRegistrationForm, validateLoginForm, isLoggedIn };
