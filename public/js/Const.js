425473c2f9734d567f90f090824818f01aa3b6a8
BartosevicBogdan <95608837+BartosevicBogdan@users.noreply.github.com>
2022-03-18 01:17:15 +0300

    endpoint consts

--- public/js/Const.js
+++ public/js/Const.js
@@ -1,4 +1,13 @@
 const baseURL = 'http://localhost:3000';
 const registerEndpoint = '/register';
+const loginEndpoint = '/login';
+const accountsEndpoint = '/accounts';
+const billsEndpoint = '/bills/';
 
-export { baseURL, registerEndpoint };
+export {
+  baseURL,
+  registerEndpoint,
+  loginEndpoint,
+  accountsEndpoint,
+  billsEndpoint,
+};
