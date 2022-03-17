4818ccd9b912aabd24013ce2c881c155c484cfd4
BartosevicBogdan <95608837+BartosevicBogdan@users.noreply.github.com>
2022-03-18 01:21:21 +0300

    bills page

---
+++ public/js/bills.js
@@ -0,0 +1,25 @@
+import { renderBills } from './modules/bills_helper.js';
+import { pushBills } from './modules/fetch.js';
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const bill_Id = urlParams.get('billID');
+
+const table = document.querySelector('table tbody');
+const groupForm = document.querySelector('#groupForm');
+
+renderBills(table, bill_Id);
+
+groupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const { amount, description } = groupForm;
+
+  const newBillData = {
+    group_id: parseInt(bill_Id),
+    amount: parseInt(amount.value),
+    description: description.value,
+  };
+
+  pushBills(newBillData);
+});
